a5a7339799ee300ab71e18b5761d4dffab887ad3
Sean McPherson <seanmcp25@gmail.com>
2018-10-03 07:03:14 -0400

    Create theme using styled-component's ThemeProvider
    Went much more smoothly than I anticipated. 👍

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
-import styled from 'styled-components'
+import styled, { ThemeProvider } from 'styled-components'
 
 import '../styles/index.css'
+import THEME from '../styles/theme'
 import Header from './header/Header'
 import Main from './Main'
 
 const Container = styled.div`
+    color: ${props => props.theme.primaryText};
     margin: 0;
 `
 
@@ -24,21 +26,23 @@ const Layout = ({ children }) => (
         }
     `}
         render={data => (
-            <Container>
-                <Helmet
-                    title={data.site.siteMetadata.title}
-                    meta={[
-                        { name: 'description', content: 'Sample' },
-                        { name: 'keywords', content: 'sample, something' },
-                    ]}
-                >
-                    <html lang="en" />
-                </Helmet>
-                <Header siteTitle={data.site.siteMetadata.title} />
-                <Main>
-                    {children}
-                </Main>
-            </Container>
+            <ThemeProvider theme={THEME}>
+                <Container>
+                    <Helmet
+                        title={data.site.siteMetadata.title}
+                        meta={[
+                            { name: 'description', content: 'Sample' },
+                            { name: 'keywords', content: 'sample, something' },
+                        ]}
+                        >
+                        <html lang="en" />
+                    </Helmet>
+                    <Header siteTitle={data.site.siteMetadata.title} />
+                    <Main>
+                        {children}
+                    </Main>
+                </Container>
+            </ThemeProvider>
         )}
     />
 )
