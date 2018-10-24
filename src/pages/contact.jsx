aa8253696379f6a87ccd8e1ae11c96bead99580f
Sean McPherson <seanmcp25@gmail.com>
2018-10-24 06:54:05 -0400

    Rework pages

---
+++ src/pages/contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// import { Link } from 'gatsby'
+
+import Layout from '../components/Layout'
+
+const Contact = () => (
+  <Layout>
+    <h1>Contact Us</h1>
+    <p>Alright mate; how are you doing?</p>
+  </Layout>
+)
+
+export default Contact
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import { Link } from 'gatsby'
+// import { Link } from 'gatsby'
 
 import Layout from '../components/Layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Graphics, illustrations, and hand-lettering</h1>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
