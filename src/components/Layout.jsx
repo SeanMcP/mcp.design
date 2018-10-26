a34933284317bad6419ff53a166055fb2956d82b
Sean McPherson <seanmcp25@gmail.com>
2018-10-26 06:59:49 -0400

    Implement AOS

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,8 +3,11 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 import styled, { ThemeProvider } from 'styled-components'
+import AOSInit from 'aos'
+import 'aos/dist/aos.css'
 
 import '../styles/index.css'
+import AOS from './common/AOS'
 import THEME from '../styles/theme'
 import Header from './header/Header'
 import Main from './Main'
@@ -14,9 +17,15 @@ const Container = styled.div`
     margin: 0;
 `
 
-const Layout = ({ children }) => (
-    <StaticQuery
-        query={graphql`
+class Layout extends React.Component {
+    componentDidMount() {
+        AOSInit.init()
+    }
+    render() {
+        const { children } = this.props;
+        return (
+            <StaticQuery
+                query={graphql`
         query SiteTitleQuery {
             site {
                 siteMetadata {
@@ -25,27 +34,64 @@ const Layout = ({ children }) => (
             }
         }
     `}
-        render={data => (
-            <ThemeProvider theme={THEME}>
-                <Container>
-                    <Helmet
-                        title={data.site.siteMetadata.title}
-                        meta={[
-                            { name: 'description', content: 'Sample' },
-                            { name: 'keywords', content: 'sample, something' },
-                        ]}
-                        >
-                        <html lang="en" />
-                    </Helmet>
-                    <Header siteTitle={data.site.siteMetadata.title} />
-                    <Main>
-                        {children}
-                    </Main>
-                </Container>
-            </ThemeProvider>
-        )}
-    />
-)
+                render={data => (
+                    <ThemeProvider theme={THEME}>
+                        <Container>
+                            <Helmet
+                                title={data.site.siteMetadata.title}
+                                meta={[
+                                    { name: 'description', content: 'Sample' },
+                                    { name: 'keywords', content: 'sample, something' },
+                                ]}
+                            >
+                                <html lang="en" />
+                            </Helmet>
+                            <Header siteTitle={data.site.siteMetadata.title} />
+                            <Main>
+                                <AOS type="fade-up">
+                                    {children}
+                                </AOS>
+                            </Main>
+                        </Container>
+                    </ThemeProvider>
+                )}
+            />
+        )
+    }
+}
+
+// const Layout = ({ children }) => (
+//     <StaticQuery
+//         query={graphql`
+//         query SiteTitleQuery {
+//             site {
+//                 siteMetadata {
+//                     title
+//                 }
+//             }
+//         }
+//     `}
+//         render={data => (
+//             <ThemeProvider theme={THEME}>
+//                 <Container>
+//                     <Helmet
+//                         title={data.site.siteMetadata.title}
+//                         meta={[
+//                             { name: 'description', content: 'Sample' },
+//                             { name: 'keywords', content: 'sample, something' },
+//                         ]}
+//                         >
+//                         <html lang="en" />
+//                     </Helmet>
+//                     <Header siteTitle={data.site.siteMetadata.title} />
+//                     <Main>
+//                         {children}
+//                     </Main>
+//                 </Container>
+//             </ThemeProvider>
+//         )}
+//     />
+// )
 
 Layout.propTypes = {
     children: PropTypes.node.isRequired,
