cc50463b9f34be7d0b5d0832f864566d41e269a5
Sean McPherson <seanmcp25@gmail.com>
2018-11-02 11:14:01 -0400

    Fix sticky header
    Added try/catch for smooth-scroll and aos.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
-var scroll = new SmoothScroll('a[href*="#"]');
-AOS.init();
+try {
+    var scroll = new SmoothScroll('a[href*="#"]');
+} catch {
+    console.log('smooth-scroll failed to load.');
+}
+try {
+    AOS.init();
+} catch {
+    console.log('aos failed to load.');
+}
+
+var header = document.querySelector('body > header');
+header.nextElementSibling.style.marginTop = `${header.offsetHeight}px`;
