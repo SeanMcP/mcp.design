39d021ba80381ebe9e8c93d731d6d19f4da66b41
Sean McPherson <seanmcp25@gmail.com>
2018-11-02 10:48:48 -0400

    Remove menu
    KISS.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,2 @@
-document.querySelector('.mobile-nav-button').addEventListener('click', toggleNavMenu);
-
-function toggleNavMenu() {
-    var menu = document.querySelector('header nav');
-    if (menu.classList.contains('open')) {
-        return menu.classList.remove('open');
-    }
-    return menu.classList.add('open');
-}
+var scroll = new SmoothScroll('a[href*="#"]');
+AOS.init();
